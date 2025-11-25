2eebf44722baddbdcd84103b26628bcd3fe278f8
Salma Yehia Ali <128738930+SalmaYehia1@users.noreply.github.com>
2025-11-25 17:01:58 +0200

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,14 @@
+// Last updated: 11/25/2025, 5:01:58 PM
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // Return empty vector if no solution is found
+    }
+};

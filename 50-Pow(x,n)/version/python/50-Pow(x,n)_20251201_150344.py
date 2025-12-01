4d4bdfbee74abcb2d9dc67982b14b358a47d3e54
Salma Yehia Ali <128738930+SalmaYehia1@users.noreply.github.com>
2025-12-01 15:03:46 +0200

    Create 50-Pow(x,n)

---
+++ 50-Pow(x,n)/version/python/50-Pow(x,n)_20251201_150344.py
@@ -0,0 +1,18 @@
+# Last updated: 12/1/2025, 3:03:44 PM
+1class Solution(object):
+2    def myPow(self, x, n):
+3        if n==0:
+4            return 1
+5        if x==0:
+6            return 0
+7
+8        if n < 0:
+9            return 1 / self.myPow(x, -n)
+10
+11        y = self.myPow(x, n // 2)
+12
+13        if n%2==0:
+14            return y*y
+15        elif n%2==1:
+16            return y*y*x
+17        

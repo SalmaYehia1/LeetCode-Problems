99c20b32a3e555a7362e6b809305bc1368e31f0f
Salma Yehia Ali <128738930+SalmaYehia1@users.noreply.github.com>
2026-03-25 16:29:59 +0200

    Create 1512-NumberofGoodPairs

---
+++ 1512-NumberofGoodPairs/version/python/1512-NumberofGoodPairs_20260325_162958.py
@@ -0,0 +1,11 @@
+# Last updated: 3/25/2026, 4:29:58 PM
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        n=len(nums)
+        ans=0
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]==nums[j]:
+                    ans=ans+1
+        return ans
+        

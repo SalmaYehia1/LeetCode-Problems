aa17a75f7abac11e182f4e6ff93b7e3a8354ad12
Salma Yehia Ali <128738930+SalmaYehia1@users.noreply.github.com>
2026-04-07 00:28:28 +0200

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -0,0 +1,12 @@
+# Last updated: 4/6/2026, 10:28:28 PM
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
+
+            
+            

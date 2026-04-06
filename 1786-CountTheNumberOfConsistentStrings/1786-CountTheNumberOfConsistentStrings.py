d4bc25aeb75fe1d67b2318aeb86e432a638bbb56
Salma Yehia Ali <128738930+SalmaYehia1@users.noreply.github.com>
2026-04-07 00:27:04 +0200

    Create 1786-CountTheNumberOfConsistentStrings

---
+++ 1786-CountTheNumberOfConsistentStrings/1786-CountTheNumberOfConsistentStrings.py
@@ -0,0 +1,11 @@
+# Last updated: 4/6/2026, 10:27:03 PM
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        allowed_set = set(allowed)
+        count = 0
+
+        for word in words:
+            if all(ch in allowed_set for ch in word):
+                count += 1
+
+        return count

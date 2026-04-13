e7376ce643ef79542845f5a06b0ea6b5b6b26cd0
Salma Yehia Ali <128738930+SalmaYehia1@users.noreply.github.com>
2026-04-13 02:02:26 +0200

    Create 1636-SortArraybyIncreasingFrequency

---
+++ 1636-SortArraybyIncreasingFrequency/version/python/1636-SortArraybyIncreasingFrequency_20260413_020225.py
@@ -0,0 +1,15 @@
+# Last updated: 4/13/2026, 2:02:25 AM
+1from collections import Counter
+2class Solution:
+3    def frequencySort(self, nums: List[int]) -> List[int]:
+4        x=Counter(nums)
+5        x =sorted(x.items(), key=lambda item: (item[1], -item[0])) #if there is a tie
+6        res=[]
+7        print(x)
+8        for i , j in x:
+9
+10            res+=[i]*j
+11        return res
+12
+13
+14        
